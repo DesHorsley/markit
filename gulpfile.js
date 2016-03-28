03cbc93019613ddcfb231c5a2a585c1bbbcf74a3
Peter Phillips <peter.phillips72@gmail.com>
2016-03-28 16:36:14 +1100

    Add gulp concat task
    concatenate markit editor js files into one file.
    edit test page to load the 1 file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-/// <binding BeforeBuild='moveSnap' />
+/// <binding BeforeBuild='moveSnap' AfterBuild='buildScripts' />
 
 // For more information on how to configure a task runner, please visit:
 // https://github.com/gulpjs/gulp
 
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat'),
+    del = require('del');
 
 var paths = {
     snap: "packages/Snap.svg.js.0.4.1/content/Scripts/snap/",
@@ -20,3 +22,14 @@ gulp.task('moveSnap', function () {
     return gulp.src(snapFiles)
         .pipe(gulp.dest(paths.snapDeployFolder));
 });
+
+gulp.task('clean', function () {
+    return del(['scripts/*-build.js']);
+});
+
+gulp.task('buildScripts', ['clean'], function () {
+    return gulp.src(['scripts/*.js', '!scripts/*-build.js'])
+    .pipe(concat('markitEditor-build.js'))
+    .pipe(gulp.dest('scripts/'));
+    
+})
