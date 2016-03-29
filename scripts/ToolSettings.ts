a601eb26e5872b973b886e631dd63cdd204dc27c
Peter Phillips <peter.phillips72@gmail.com>
2016-03-29 20:25:00 +1100

    add fill property to toolsettngs
    changed stroke from any to string

--- scripts/ToolSettings.ts
+++ scripts/ToolSettings.ts
@@ -14,14 +14,16 @@ module markit {
         public width: number;
         public height: number;
         public strokeWidth: number;
-        public stroke: any;
-        
+        public stroke: string;
+        public fill: string;
+
         constructor() {
             this.commandMode = CommandMode.Select;
             this.width = 1;
             this.height = 1;
             this.strokeWidth = 1;
             this.stroke = "#000";
+            this.fill = "none";
         }       
     
     }
