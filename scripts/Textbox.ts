9a55139337611103b5109b533a1fe6193b87bda6
Peter Phillips <peter.phillips72@gmail.com>
2016-05-15 01:12:23 +1000

    text resizing and dragging improvements

--- scripts/Textbox.ts
+++ scripts/Textbox.ts
@@ -4,131 +4,111 @@ module markit {
     export class Textbox extends Rectangle {
 
         protected _text: Snap.Element;
-        protected _mask: HTMLElement;
-        
+        protected _mask: HTMLElement;        
+        protected _resizing: boolean;
+
         public get text(): Snap.Element {
             return this._text;
         }
-       
-        public drawComplete(selected?: boolean): void {
-            //this.textControl = new TextControl(this);
+
+        constructor(paper: Snap.Paper, origin: Point, toolSettings: ToolSettings) {
+            super(paper, origin, toolSettings);
+            this._resizing = false;
+            this._text = null;
+            this._mask = null;
+        }
+
+        public drawComplete(selected?: boolean): void {            
             super.drawComplete();
-            if (typeof this._mask == "undefined" || this._mask == null) {
-                this._mask = this.createMask();
-                this._mask.focus();
-            }                   
+            if (this._mask == null) {               
+                this.createMask();
+                this._mask.focus();                               
+            }                              
+        }
+
+        public select(): void {
+            super.select();
+            if (this._mask == null) {
+                this.createMask();
+                if (this._text != null) {
+                    this.transferTextToEditor();
+                }                
+            }
+        }
+
+        public deselect(): void {
+            super.deselect();
+            this.closeEditor();
         }
 
         public redraw(mode: string, offset: Point, handleIndex?: number): void {
             super.redraw(mode, offset, handleIndex);
-            if (typeof this._mask != "undefined" && this._mask != null) {
-                let rect = this.getBoundingRect();
-                let svg = DomServices.getSVG();
-                let canvasRect = svg.getBoundingClientRect();
-                let borderTop = parseInt(svg.style.borderTopWidth);
-                let borderLeft = parseInt(svg.style.borderLeftWidth);
-
-                let maskTop = canvasRect.top + borderTop + rect.y + this.toolSettings.strokeWidth;
-                let maskLeft = canvasRect.left + rect.x + borderLeft + this.toolSettings.strokeWidth;
-                let maskWidth = rect.width - this.toolSettings.strokeWidth * 2;
-                let maskHeight = rect.height - this.toolSettings.strokeWidth * 2;
-                
-                let str = `top: ${maskTop}px; left: ${maskLeft}px; width: ${maskWidth}px; height: ${maskHeight}px`;
-                this._mask.style.cssText = str;
-            }
-            if (typeof this._text != "undefined" && this._text != null) {
-                if (mode == "drag") {
-                    let rect = this.getBoundingRect();
-                    let x = rect.x + this.toolSettings.strokeWidth + 3;
-                    let y = rect.y + this.toolSettings.strokeWidth + 12 + (12 / 3)
-                    this._text.attr({ x: rect.x + this.toolSettings.strokeWidth, y: rect.y + this.toolSettings.strokeWidth });
-
-                    for (let i = 0; i < this._text.node.children.length; i++) {
-                        if (i == 0) {
-                            (this._text.node.children[i] as SVGTSpanElement).setAttribute("x", x.toString());
-                            (this._text.node.children[i] as SVGTSpanElement).setAttribute("y", y.toString());
-                        }
-                        else {
-                            (this._text.node.children[i] as SVGTSpanElement).setAttribute("x", x.toString());
-                        }
-                    }
-                }
-                else if (mode == "resize") {
-                    let str = this.getText();
-                    this.removeTextLines();
-                    let rect = this.getBoundingRect();
-                    this._text.attr({ x: rect.x + this.toolSettings.strokeWidth, y: rect.y + this.toolSettings.strokeWidth });
-                    let span = DomServices.createSpan();
-                    let x = rect.x + this.toolSettings.strokeWidth + 3;
-                    let y = rect.y + this.toolSettings.strokeWidth + 12 + (12 / 3);
-                    span.setAttribute("x", x.toString());
-                    span.setAttribute("y", y.toString());
-                    this._text.node.appendChild(span);
-                    this.insertText(str);
-                }
-            }
+            this.positionMask();            
         }
 
         public containsElement(element: Element): boolean {
-            
+
             if (this._text != null) {
                 if (this._text.node === element) {
                     return true;
-                }
+                }                
                 for (let i = 0; i < this._text.node.children.length; i++) {
                     if (this._text.node.children[i] === element) {
                         return true;
                     }
-                }
-            }           
-            return super.containsElement(element);
+                }                               
+            }
+            return super.containsElement(element);                              
         }
 
-        protected createMask(): HTMLElement {
+        protected createMask(): void {
+            this._mask = DomServices.createDIV();            
+            this._mask.contentEditable = "true";
+            this._mask.className = "markit-textmask"; 
+            this.positionMask();           
+            DomServices.getSVG().parentNode.insertBefore(this._mask, DomServices.getSVG().nextSibling);            
+        }
 
-            let mask = DomServices.createDIV();
-            let rect = this.getBoundingRect();
-            let borderWidth = parseInt(this._element.attr("strokeWidth"));
-            
+        protected positionMask(): void {
+
+            let rect = this.getBoundingRect(); 
             let svg = DomServices.getSVG();
             let canvasRect = svg.getBoundingClientRect();
             let borderTop = parseInt(svg.style.borderTopWidth);
             let borderLeft = parseInt(svg.style.borderLeftWidth);
 
-            let maskTop = canvasRect.top + borderTop + rect.y + borderWidth;
-            let maskLeft = canvasRect.left + rect.x + borderLeft + borderWidth;
-            let maskWidth = rect.width - borderWidth * 2;
-            let maskHeight = rect.height - borderWidth * 2;
-
-            mask.contentEditable = "true";
-            mask.className = "markit-textmask";
+            let maskTop = canvasRect.top + rect.y + borderTop + this.toolSettings.strokeWidth;
+            let maskLeft = canvasRect.left + rect.x + this.toolSettings.strokeWidth;
+            //let maskLeft = canvasRect.left + rect.x + borderLeft + this.toolSettings.strokeWidth;
+            let maskWidth = rect.width - this.toolSettings.strokeWidth * 2;
+            let maskHeight = rect.height - this.toolSettings.strokeWidth * 2;
+            let log = `rect width: ${rect.width}, rect left: ${rect.x}, maskWidth: ${maskWidth}, maskLeft: ${maskLeft}`
+            console.log(log);
+            this._mask.contentEditable = "true";
+            this._mask.className = "markit-textmask";
             let str = `top: ${maskTop}px; left: ${maskLeft}px; width: ${maskWidth}px; height: ${maskHeight}px`;
-            mask.style.cssText = str;
-            let closeIcon = this.createMaskCloseIcon();
-            mask.appendChild(closeIcon);
-            DomServices.getSVG().parentNode.insertBefore(mask, DomServices.getSVG().nextSibling);                        
-            return mask;
+            this._mask.style.cssText = str;
         }
 
-        protected hide(): void {
-            this.removeMaskEventHandlers();
-            DomServices.getSVG().parentNode.removeChild(this._mask);
-            this._mask = null;
+        protected removeMask(): void {            
+            DomServices.getSVG().parentNode.removeChild(this._mask);            
+            this._mask = null;            
         }             
-        
+
+        protected transferTextToEditor(): void {
+            let str = this.getText();
+            this._mask.textContent = str;
+            this.removeTextLines();
+        }
+
         protected closeEditor(): void {
             
             let coords = this.getBoundingRect();
-            if (typeof this._text == "undefined" || this._text == null) {
-                
+            if (this._text == null) {                
                 let border = this.toolSettings.strokeWidth;
-                this._text = this.paper.text(coords.x + border, coords.y + border, "");
-                
+                this._text = this.paper.text(coords.x + border, coords.y + border, "");                
             }
-            while (this._text.node.childNodes.length > 0) {
-                this._text.node.removeChild(this._text.node.firstChild);
-            }
-
+            this.removeTextLines();            
             let span = DomServices.createSpan();
             let x = coords.x + this.toolSettings.strokeWidth + 3;
             let y = coords.y + this.toolSettings.strokeWidth + 12 + (12/3);
@@ -136,26 +116,19 @@ module markit {
             span.setAttribute("y", y.toString());
             this._text.node.appendChild(span);
             this.insertText(this._mask.textContent);            
-            this.hide();
+            this.removeMask();
         }
 
         protected insertText(text: string): void {
 
             let words = text.split(" ");
             let rect = this.getBoundingRect();
-
             let spanWidth = rect.width - (this.toolSettings.strokeWidth * 2) - 6;
+            let str = `rect width: ${rect.width}, strokeWidth: ${this.toolSettings.strokeWidth}, spanWidth: ${spanWidth}`;
+            console.log(str);
             let span = this._text.node.firstChild as SVGTSpanElement;
             for (let i = 0; i < words.length; i++) {
                 span.innerHTML += words[i] + " ";
-
-                let str = "word tested: " + words[i] + "\r\n";
-                str += "parent client width: " + span.parentElement.clientWidth;
-                str += "\r\nspan client Width: " + span.clientWidth;
-                str += "\r\nspan boundingClientRect width: " + span.getBoundingClientRect().width;
-                str += "\r\nspan computed text length: " + span.getComputedTextLength();
-
-                console.log(str);
 
                 if (span.getComputedTextLength() >= spanWidth) {
                     span.innerHTML = span.innerHTML.substr(0, span.innerHTML.length - (words[i].length + 1));
@@ -196,25 +169,7 @@ module markit {
                 height: height
             };
             return rect;
-        }
-
-        protected createMaskCloseIcon(): HTMLImageElement {
-
-            let closeIcon = DomServices.createImage();
-            closeIcon.src = "content/window-close-icon.png";
-            closeIcon.className = "markit-close-window";
-            closeIcon.onclick = this.closeEditor.bind(this);
-            return closeIcon;
-        }        
-
-        protected removeMaskEventHandlers(): void {
-           let list = this._mask.getElementsByClassName("markit-close-window");
-            if (list.length > 0) {
-                if (list[0] instanceof HTMLImageElement) {
-                    (list[0] as HTMLImageElement).onclick = null;
-                }
-            }
-        }
+        }       
        
     }
 }
